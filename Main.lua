ef846f6e519a79139d0d4fdd4e04b6f265c4f908
13AM <79609111+Covarun@users.noreply.github.com>
2022-07-01 12:00:28 -0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 1962086868 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/TowerOfHell.lua",true))()
+  elseif game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/Prison%20life.lua",true))()
+  end)
