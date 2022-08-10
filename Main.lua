9cf797ffa5305ef467cba802fe312e6c8fb2e321
13AM <79609111+Covarun@users.noreply.github.com>
2022-08-10 16:45:55 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,13 @@
 if game.PlaceId == 1962086868 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/TowerOfHell.lua",true))()
-  elseif game.PlaceId == 155615604 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/Prison%20life.lua",true))()
-  end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/TowerOfHell.lua"))()
+    
+    elseif game.PlaceId == 155615604 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/Prison%20life.lua"))()
+    
+    elseif game.PlaceId == 3101667897 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/LegendsOfSpeed.lua"))()
+    
+    elseif game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/Arsenal.lua"))()
+
+end
