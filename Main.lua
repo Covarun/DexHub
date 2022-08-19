125ac7b86572806391dabe293a980b40154d4f87
13AM <79609111+Covarun@users.noreply.github.com>
2022-08-18 21:52:15 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9,5 +9,8 @@ if game.PlaceId == 1962086868 then
     
     elseif game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/Arsenal.lua"))()
+  
+    elseif game.PlaceId == 5166670285 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Covarun/DexHub/main/BadgeWalk.lua"))()
 
 end
